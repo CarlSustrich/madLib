6be2bcee7e51910779e0e235332abb7eb8d34072
Carl Sustrich <113807749+CarlSustrich@users.noreply.github.com>
2023-02-28 09:32:58 -0800

    end of day WIP

--- MadLib/Controllers/HomeController.cs
+++ MadLib/Controllers/HomeController.cs
@@ -21,30 +21,14 @@ namespace MadLib.Controllers
     [Route("/TolkienMadLib")]
     public ActionResult TolkienMadLib(string fruit,string pluralnoun1, string noun1, string noun2, string pluralnoun2, string noun3, string sillyword)
     {
-      LetterVariable newMadLib = new LetterVariable();
-      newMadLib.Fruit = fruit;
-      newMadLib.PluralNoun1 = pluralnoun1;
-      newMadLib.Noun1 = noun1;
-      newMadLib.Noun2 = noun2;
-      newMadLib.PluralNoun2 = pluralnoun2;
-      newMadLib.Noun3 = noun3;
-      newMadLib.SillyWord = sillyword;
+      MadderLib newMadLib = new MadderLib(fruit, pluralnoun1, noun1, noun2, pluralnoun2, noun3, sillyword);
       return View(newMadLib);
     }
     
     [Route("/CodingMadLib")]
     public ActionResult CodingMadLib(string Adjective1, string ProgrammingLanguage, string Noun, string Number, string ProgrammingConcept, string PluralNoun, string Adverb, string Adjective3, string Adjective2)
     {
-      CodingVariable newMadLib = new CodingVariable();
-      newMadLib.Adjective1 = Adjective1;
-      newMadLib.ProgrammingLanguage = ProgrammingLanguage; 
-      newMadLib.Noun = Noun; 
-      newMadLib.Number = Number; 
-      newMadLib.ProgrammingConcept = ProgrammingConcept; 
-      newMadLib.PluralNoun = PluralNoun; 
-      newMadLib.Adverb = Adverb; 
-      newMadLib.Adjective3 = Adjective3; 
-      newMadLib.Adjective2 = Adjective2; 
+      MadderLib newMadLib = new MadderLib(Adjective1, ProgrammingLanguage, Noun, Number, ProgrammingConcept, PluralNoun, Adverb, Adjective3, Adjective2);
       return View(newMadLib);
 
     }
@@ -52,15 +36,7 @@ namespace MadLib.Controllers
     [Route("/DragonAgeMadLib")]
     public ActionResult DragonAgeMadLib(string DARace, string Animal, string Name, string Adverb1, string Adjective2, string DAClass, string Adjective3, string DATerm)
     {
-      DAVariable newMadLib  = new DAVariable();
-      newMadLib.DARace = DARace;
-      newMadLib.Animal = Animal;
-      newMadLib.Name = Name;
-      newMadLib.Adverb1 = Adverb1;
-      newMadLib.Adjective2 = Adjective2;
-      newMadLib.DAClass = DAClass;
-      newMadLib.Adjective3 = Adjective3;
-      newMadLib.DATerm = DATerm;
+      MadderLib newMadLib  = new MadderLib(DARace, Animal, Name, Adverb1, Adjective2, Adjective3, DAClass, DATerm);
       return View(newMadLib);
     }
   }
--- MadLib/Models/MadLib.cs
+++ MadLib/Models/MadLib.cs
@@ -1,26 +1,7 @@
 namespace MadLib.Models
 {
 
-  // public class MadLib 
-  // {
-  //   public string Fruit {get;set;}
-  //   public string PluralNoun1 {get;set;}
-  //   public string Noun1 { get; set; }
-  //   public string Noun2 {get;set;}
-  //   public string PluralNoun2 {get;set;}
-  //   public string Noun3 { get; set; }
-  //   public string SillyWord {get;set;}
-  //   public string Adjective1 {get;set;}
-  //   public string ProgrammingLanguage {get;set;}
-  //   public string Noun { get; set; }
-  //   public string Number {get;set;}
-  //   public string ProgrammingConcept {get;set;}
-  //   public string PluralNoun { get; set; }
-  //   public string Adverb {get;set;}
-  //   public string Adjective3 {get;set;}
-  //   public string Adjective2 {get;set;}
-  // }
-  public class LetterVariable
+  public class MadderLib 
   {
     public string Fruit {get;set;}
     public string PluralNoun1 {get;set;}
@@ -29,10 +10,6 @@ namespace MadLib.Models
     public string PluralNoun2 {get;set;}
     public string Noun3 { get; set; }
     public string SillyWord {get;set;}
-  }
-
-  public class CodingVariable
-  {
     public string Adjective1 {get;set;}
     public string ProgrammingLanguage {get;set;}
     public string Noun { get; set; }
@@ -42,18 +19,48 @@ namespace MadLib.Models
     public string Adverb {get;set;}
     public string Adjective3 {get;set;}
     public string Adjective2 {get;set;}
-  
-  }
-
-  public class DAVariable
-  {
     public string DARace {get;set;}
     public string Animal {get;set;}
     public string Name {get;set;}
     public string Adverb1 {get;set;}
-    public string Adjective2 {get;set;}
     public string DAClass {get;set;}
-    public string Adjective3 {get;set;}
     public string DATerm {get;set;}
+
+    public MadderLib (string fruit, string pluralnoun1, string noun1, string noun2, string pluralnoun2, string noun3, string sillyword)
+    {
+    
+      Fruit = fruit;
+      PluralNoun1 = pluralnoun1;
+      Noun1 = noun1;
+      Noun2 = noun2;
+      PluralNoun2 = pluralnoun2;
+      Noun3 = noun3;
+      SillyWord = sillyword;
+    }
+
+    public MadderLib (string Adjective1, string ProgrammingLanguage, string Noun, string Number, string ProgrammingConcept, string PluralNoun, string Adverb, string Adjective3, string Adjective2)
+    {
+      this.Adjective1 = Adjective1;
+      this.ProgrammingLanguage = ProgrammingLanguage;
+      this.Noun = Noun;
+      this.Number = Number;
+      this.ProgrammingConcept = ProgrammingConcept;
+      this.PluralNoun = PluralNoun;
+      this.Adverb = Adverb;
+      this.Adjective3 = Adjective3;
+      this.Adjective2 = Adjective2;
+    }
+    public MadderLib (string DARace, string Animal, string Name, string Adverb1, string Adjective2, string DAClass, string Adjective3, string DATerm)
+    {
+    this.DARace = DARace;
+    this.DATerm = DATerm;
+    this.Adjective3 = Adjective3;
+    this.Adjective2 = Adjective2;
+    this.Adjective1 = Adjective1;
+    this.Animal = Animal;
+    this.Name = Name;
+    }
+
+  
   }
 }
